78b829276919d3b58339bd65357085d6f706054c
HuMarinho <humbertombmarinho@gmail.com>
2022-11-25 20:46:15 -0300

    substituido let por const

--- velocidade.js
+++ velocidade.js
@@ -2,6 +2,6 @@ const distancia = 500;
 const tempo = 10;
 
 const velocidade = distancia / tempo;
-let velocidadems = velocidade * 3.6;
+const velocidadems = velocidade * 3.6;
 console.log(velocidadems);
 
