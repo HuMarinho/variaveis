0a77611ec9af41681be3e48738869626ddcb684d
HuMarinho <humbertombmarinho@gmail.com>
2022-11-25 11:15:40 -0300

    resolução 5

---
+++ distancia.js
@@ -0,0 +1,15 @@
+const x = 1;
+const x1 = 1;
+
+const y = 1;
+const y1 = 4;
+
+let ponto1 = Math.pow((x1 - x), 2);
+let ponto2 = Math.pow((y1 - y), 2);
+
+const distancia = Math.sqrt(ponto1 + ponto2);
+console.log(distancia);
+
+
+
+
