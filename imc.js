26bd418435969d5711edc33d1dd4a3a77bcebf94
HuMarinho <humbertombmarinho@gmail.com>
2022-11-25 19:58:21 -0300

    acrescentado mensagem no log e fixado duas casas decimais.

--- imc.js
+++ imc.js
@@ -1,5 +1,5 @@
 const peso = 95;
 const altura = 1.92;
 
-const imc = peso / (altura * altura);
-console.log(imc);
+const imc = peso / (altura * altura); // poderia ter usado altura ** 2; Math.pow
+console.log(`seu IMC é ${imc.toFixed(2)}`);
