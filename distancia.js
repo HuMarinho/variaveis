27ef9519ead6bc5803f01de7af0129c0cec22b52
HuMarinho <humbertombmarinho@gmail.com>
2022-11-25 20:42:59 -0300

    alternativa a resolver a potencia fora da const distancia sem atribuiçoes de novas variáveis

--- distancia.js
+++ distancia.js
@@ -4,12 +4,8 @@ const x1 = 1;
 const y = 1;
 const y1 = 4;
 
-let ponto1 = Math.pow((x1 - x), 2);
+let ponto1 = Math.pow((x1 - x), 2); //toda essa operação poderia vir dento da const de distancia 
 let ponto2 = Math.pow((y1 - y), 2);
 
-const distancia = Math.sqrt(ponto1 + ponto2);
+const distancia = Math.sqrt(ponto1 + ponto2); // Math.sqrt ( ( x1 -x ) ** 2 + ( y1 - y )** 2)
 console.log(distancia);
-
-
-
-
