c07df026861e486997d0852e825d9d904dd27095
HuMarinho <humbertombmarinho@gmail.com>
2022-11-25 20:19:21 -0300

    substituido o let por const e fixado em duas casas decimais

--- montante.js
+++ montante.js
@@ -1,8 +1,7 @@
-let capital = 1000;
-let taxa = (1 + (12.5 / 100));
-let tempo = 5;
+const capital = 1000;
+const taxa = (1 + (12.5 / 100));
+const tempo = 5;
 
 const montante = capital * Math.pow(taxa, tempo);
 
-console.log(montante);
-
+console.log(montante.toFixed(2));
